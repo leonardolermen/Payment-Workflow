d1a8e4cb3df40cfa5ff4d5a9482efb80230c128b
hssHigor <higorhss@outlook.com>
2026-04-07 15:12:15 -0300

    Feat create user entity

---
+++ core-service/src/main/java/com/payflow/coreservice/model/User.java
@@ -0,0 +1,53 @@
+package com.payflow.coreservice.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String document;
+
+    @Column(nullable = false)
+    private String documentType;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+
+
+    @PrePersist
+    public void prePersist() {
+        if (uuid == null) {
+            uuid = UUID.randomUUID();
+        }
+    }
+
+}
--- core-service/src/main/resources/db/migration/V1__init.sql
+++ core-service/src/main/resources/db/migration/V1__init.sql
@@ -1,3 +1,24 @@
 -- V1__init.sql
 -- Baseline migration for core-service
 -- Add your DDL scripts here (e.g., CREATE TABLE statements)
+CREATE TABLE users (
+                       id BIGSERIAL PRIMARY KEY,
+
+                       uuid UUID NOT NULL UNIQUE,
+
+                       name VARCHAR(255) NOT NULL,
+
+                       email VARCHAR(255) NOT NULL UNIQUE,
+
+                       password VARCHAR(255) NOT NULL,
+
+                       document VARCHAR(50) NOT NULL UNIQUE,
+
+                       document_type VARCHAR(50) NOT NULL,
+
+                       balance NUMERIC(19, 2) NOT NULL,
+
+                       status VARCHAR(50) NOT NULL,
+
+                       created_at TIMESTAMP NOT NULL
+);
